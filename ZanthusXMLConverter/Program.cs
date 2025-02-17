64b9f22c8f1e2afa77c1e8fd76ef9884ee554629
Daniel <Daniel@DESKTOP-MID5JJ0>
2025-02-17 14:12:12 -0300

    ALteração do projeto para .cs

---
+++ ZanthusXMLConverter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+
+namespace IntegracaoRequester {
+	class Program {
+		static void Main() {
+			var appSettings = ConfigurationManager.AppSettings;
+			ConsoleKeyInfo pressedKey;
+
+			Console.WriteLine(" - Pressione [Y] para enviar uma requisição");
+			Console.WriteLine(" - Pressione [Esc] para sair");
+
+			do {
+				pressedKey = Console.ReadKey();
+
+				switch (pressedKey.Key) {
+					default:
+						break;
+					case ConsoleKey.Y:
+						Console.WriteLine("\n\n----------------------------------------");
+						Console.WriteLine("Enviando requisição...\n\n");
+						RequestMerchandise.SearchForMerchandises(
+							(appSettings.Get("requestEndpoint") + appSettings.Get("requestMethod")),
+							(appSettings.Get("requestXMLFilePath") + appSettings.Get("requestXMLFileName"))
+						);
+						break;
+				}
+			} while (pressedKey.Key != ConsoleKey.Escape);
+		}
+	}
+}

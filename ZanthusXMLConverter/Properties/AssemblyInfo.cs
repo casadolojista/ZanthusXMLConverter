96ab66bddafecd0ef205294582308e56e5c8b937
Daniel <Daniel@DESKTOP-MID5JJ0>
2025-02-17 16:21:03 -0300

    Fix

--- ZanthusXMLConverter/Properties/AssemblyInfo.cs
+++ ZanthusXMLConverter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("XMLConverter")]
+[assembly: AssemblyTitle("ZanthusXMLConverter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XMLConverter")]
+[assembly: AssemblyProduct("ZanthusXMLConverter")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
